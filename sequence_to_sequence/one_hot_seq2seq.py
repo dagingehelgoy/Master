db1272ccc3eb9370555254d03fcb5dd6cf8ca92c
Dag Inge Helgøy <dihelgoy@gmail.com>
2017-04-03 11:14:43 +0200

    Restructure repository Add seq2seq using embeddings and one hot

---
+++ sequence_to_sequence/one_hot_seq2seq.py
@@ -0,0 +1,162 @@
+import os
+
+import keras
+import numpy
+from keras.callbacks import EarlyStopping
+from keras.layers import LSTM, RepeatVector, Embedding, TimeDistributed, Dense
+from keras.models import Sequential, model_from_json
+from keras.preprocessing import sequence
+from keras.preprocessing.text import Tokenizer
+from keras.utils.np_utils import to_categorical
+
+from enums import EmbToOnehotConf
+from seq2seq import get_word_embeddings, set_model_name
+from sequence_to_sequence.encoder_decoder_model_checkpoint import EncoderDecoderModelCheckpoint
+
+
+def open_corpus():
+	sentence_file = open("data/datasets/Flickr30k.txt")
+	lines = sentence_file.readlines()
+	sentence_file.close()
+	return lines
+
+
+def batch_generator(data, batch_size, nb_vocab):
+	while 1:
+		for pos in range(0, len(data), batch_size):
+			Xs = []
+			Ys = []
+			for i in range(pos, pos + batch_size):
+				if i < len(data):
+					X = data[i]
+					Xs.append(X)
+					Y = to_categorical(X, nb_vocab)
+					Ys.append(Y)
+			yield (numpy.asarray(Xs), numpy.asarray(Ys))
+
+
+def get_word_embedding_matrix(word_to_id, embedding_dim, word_embedding_method):
+	embeddings_index = get_word_embeddings(word_embedding_method)
+	embedding_matrix = numpy.zeros((len(word_to_id) + 1, embedding_dim))
+	for word, i in word_to_id.items():
+		embedding_vector = embeddings_index.get(word)
+		if embedding_vector is not None:
+			# words not found in embedding index will be all-zeros.
+			embedding_matrix[i] = embedding_vector
+
+
+def get_model(conf):
+	encoder = Sequential()
+	embedding_layer = Embedding(conf.NB_WORDS + 2, conf.EMBEDDING_DIMENSION, input_length=conf.MAX_SEQUENCE_LENGTH,
+								trainable=True, mask_zero=True)
+	encoder.add(embedding_layer)
+	encoder.add(LSTM(output_dim=conf.HIDDEN_DIM, input_shape=(conf.MAX_SEQUENCE_LENGTH, conf.EMBEDDING_DIMENSION),
+					 return_sequences=False))
+	encoder.add(RepeatVector(conf.MAX_SEQUENCE_LENGTH))  # Get the last output of the RNN and repeats it
+	decoder = Sequential()
+	decoder.add(
+		LSTM(output_dim=conf.HIDDEN_DIM, input_shape=(conf.MAX_SEQUENCE_LENGTH, conf.HIDDEN_DIM),
+			 return_sequences=True))
+	for _ in range(1, conf.DECODER_HIDDEN_LAYERS):
+		decoder.add(LSTM(output_dim=conf.HIDDEN_DIM, return_sequences=True))
+	decoder.add(TimeDistributed(Dense(output_dim=conf.NB_WORDS, input_shape=(conf.MAX_SEQUENCE_LENGTH, conf.HIDDEN_DIM),
+									  activation=conf.OUTPUT_LAYER_ACTIVATION)))
+	model = Sequential()
+	model.add(encoder)
+	model.add(decoder)
+	model.summary()
+	return decoder, encoder, model
+
+
+def predict_embedding_to_onehot(conf, data, id_to_word, test_model):
+	test_gen = batch_generator(data[:-conf.VAL_DATA_SIZE], conf.BATCH_SIZE, conf.NB_WORDS)
+	test_data = test_gen.next()[0]
+	preds = test_model.predict(test_data, verbose=0)
+	for batch_index in range(len(preds)):
+		print "Input sentence " + str(batch_index + 1)
+		for word in test_data[batch_index]:
+			if word == 0:
+				print "0 ",
+			else:
+				print id_to_word[word] + " ",
+		print
+		for word in preds[batch_index]:
+			argmax = numpy.argmax(word)
+			if argmax == 0:
+				print "0 ",
+			else:
+				print id_to_word[argmax] + " ",
+		print
+
+
+def inference(conf, id_to_word, model_filename, sequences, weights_filename):
+	filename = "text_generators/logs/" + model_filename + "/weights/" + weights_filename
+	saved_model_path = "text_generators/logs/" + model_filename + "/model.json"
+	if os.path.exists(saved_model_path):
+		with open(saved_model_path, "r") as json_file:
+			loaded_model_json = json_file.read()
+		test_model = model_from_json(loaded_model_json)
+	else:
+		_, _, model = get_model(conf)
+		test_model = model
+	test_model.load_weights(filename)
+	data = sequence.pad_sequences(sequences, conf.MAX_SEQUENCE_LENGTH)
+	numpy.random.shuffle(data)
+	predict_embedding_to_onehot(conf, data, id_to_word, test_model)
+
+
+def train_model(conf, sequences):
+	decoder, encoder, model = get_model(conf)
+
+	model.compile(metrics=['accuracy'], loss=conf.LOSS, optimizer='adam')
+	filepath, log_dir, log_folder = set_model_name(conf)
+	data = sequence.pad_sequences(sequences, conf.MAX_SEQUENCE_LENGTH)
+	numpy.random.shuffle(data)
+	val_gen = batch_generator(data[-conf.VAL_DATA_SIZE:], conf.BATCH_SIZE, conf.NB_WORDS)
+	train_gen = batch_generator(data[:-conf.VAL_DATA_SIZE], conf.BATCH_SIZE, conf.NB_WORDS)
+	if 'SSH_CONNECTION' in os.environ.keys():
+		tensorboard = keras.callbacks.TensorBoard(log_dir='text_generators/logs/' + log_folder, histogram_freq=1,
+												  write_graph=True)
+		es = EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=0, mode='auto')
+		checkpoint = EncoderDecoderModelCheckpoint(decoder, encoder, start_after_epoch=10, filepath=filepath,
+												   monitor='val_loss', verbose=1, save_best_only=True, mode='min',
+												   period=3)
+		callbacks_list = [checkpoint, tensorboard, es]
+		model.fit_generator(generator=train_gen, samples_per_epoch=len(data) - conf.VAL_DATA_SIZE,
+							validation_data=val_gen, nb_val_samples=conf.VAL_DATA_SIZE, nb_epoch=conf.EPOCHS,
+							callbacks=callbacks_list)
+
+		model_json = model.to_json()
+		with open(log_dir + log_folder + "/model.json", "w") as json_file:
+			json_file.write(model_json)
+	else:
+
+		checkpoint = EncoderDecoderModelCheckpoint(decoder, encoder, start_after_epoch=2, filepath=filepath,
+												   monitor='val_loss', verbose=1, save_best_only=True,
+												   mode='min',
+												   period=3)
+		from keras.utils.visualize_util import plot
+		plot(model, show_shapes=True, to_file='text_generators/logs/' + log_folder + "/model.png")
+		model.fit_generator(train_gen, len(data), conf.EPOCHS, callbacks=[checkpoint])
+
+
+def one_hot_seq2seq(train=True):
+	conf = EmbToOnehotConf
+
+	lines = open_corpus()
+	lines = ['<sos> ' + line + ' <eos> <pad>' for line in lines]
+	tokenizer = Tokenizer(filters="""!"#$%&'()*+-/:;=?@[\]^_`{|}~""")
+	tokenizer.fit_on_texts(lines)
+	sequences = tokenizer.texts_to_sequences(lines)
+
+	word_to_id = tokenizer.word_index
+	id_to_word = {token: idx for idx, token in word_to_id.items()}
+
+	if train:
+		train_model(conf, sequences)
+
+	else:
+		model_filename = "S2S_2EMB_2017-03-27_VS2+1000_BS128_HD10_DHL1_ED20_WEMword2vec"
+		weights_filename = "E:131-L:0.0294.hdf5"
+
+		inference(conf, id_to_word, model_filename, sequences, weights_filename)
---
+++ sequence_to_sequence/seq2seq.py
@@ -0,0 +1,279 @@
+import os
+import random
+from datetime import datetime
+
+import keras
+import numpy as np
+import tensorflow as tf
+from keras import metrics
+from keras.callbacks import EarlyStopping
+from keras.layers import LSTM, RepeatVector, TimeDistributed, Dense
+from keras.models import Sequential, model_from_json
+from sklearn.metrics.pairwise import cosine_similarity
+
+from enums import W2VEmbToEmbConf
+from helpers.io_helper import load_pickle_file
+from helpers.list_helpers import print_progress
+from sequence_to_sequence.encoder_decoder_model_checkpoint import EncoderDecoderModelCheckpoint
+
+
+def contrastive_loss(y_true, y_pred):
+	shape = tf.shape(y_true)  # a list: [None, 9, 2]
+	dim = tf.mul(shape[1], shape[2])  # dim = prod(9,2) = 18
+	y_true = tf.reshape(y_true, [-1, dim])  # -1 means "all"
+	y_pred = tf.reshape(y_pred, [-1, dim])  # -1 means "all"
+	x2 = tf.expand_dims(tf.transpose(y_pred, [0, 1]), 1)
+	y2 = tf.expand_dims(tf.transpose(y_true, [0, 1]), 0)
+	diff = y2 - x2
+	maximum = tf.maximum(diff, 0.0)
+	tensor_pow = tf.square(maximum)
+	errors = tf.reduce_sum(tensor_pow, 2)
+	diagonal = tf.diag_part(errors)
+	cost_s = tf.maximum(0.05 - errors + diagonal, 0.0)
+	cost_im = tf.maximum(0.05 - errors + tf.reshape(diagonal, (-1, 1)), 0.0)
+	cost_tot = cost_s + cost_im
+	zero_diag = tf.mul(diagonal, 0.0)
+	cost_tot_diag = tf.matrix_set_diag(cost_tot, zero_diag)
+	tot_sum = tf.reduce_sum(cost_tot_diag)
+	return tot_sum
+
+
+def batch_generator(data, conf):
+	while 1:
+		for pos in range(0, len(data), conf.BATCH_SIZE):
+			Xs = np.zeros((conf.BATCH_SIZE, conf.MAX_SEQUENCE_LENGTH, conf.EMBEDDING_DIMENSION))
+			for i in range(pos, pos + conf.BATCH_SIZE):
+				if i < len(data):
+					X = np.zeros((conf.MAX_SEQUENCE_LENGTH, conf.EMBEDDING_DIMENSION))
+					for j in range(len(data[i][:conf.MAX_SEQUENCE_LENGTH])):
+						X[j] = data[i][j]
+					Xs[i % conf.BATCH_SIZE] = X
+			yield (Xs, Xs)
+
+
+def get_word_embeddings(conf):
+	if conf.WORD_EMBEDDING_METHOD == 'glove':
+		embeddings_index = {}
+		f = open('data/embeddings/glove.6B.300d.txt')
+		count = 0
+		for line in f:
+			values = line.split()
+			word = values[0]
+			coefs = np.asarray(values[1:], dtype='float32')
+			embeddings_index[word] = coefs
+			count += 1
+			if count % 100 == 0:
+				print_progress(count, 400000, prefix="Getting glove word embeddings")
+		f.close()
+		return embeddings_index
+
+	elif conf.WORD_EMBEDDING_METHOD == 'word2vec':
+		embedding_dict_name = "data/embeddings/word2vec_embedding_dict_" + str(conf.EMBEDDING_DIMENSION) + ".pkl"
+		return load_pickle_file(embedding_dict_name)
+
+	print("WORD_EMBEDDING_METHOD not found")
+	return None
+
+
+def get_word_embedding_matrix(word_to_id, embedding_dim, word_embedding_method):
+	embeddings_index = get_word_embeddings(word_embedding_method)
+	embedding_matrix = np.zeros((len(word_to_id) + 1, embedding_dim))
+	for word, i in word_to_id.items():
+		embedding_vector = embeddings_index.get(word)
+		if embedding_vector is not None:
+			# words not found in embedding index will be all-zeros.
+			embedding_matrix[i] = embedding_vector
+
+
+def inference(conf, inference_sentences, inference_vectors, model_filename, weights_filename, word_embeddings):
+	filename = "text_generators/logs/" + model_filename + "/weights/" + weights_filename
+	saved_model_path = "text_generators/logs/" + model_filename + "/model.json"
+	if os.path.exists(saved_model_path):
+		with open(saved_model_path, "r") as json_file:
+			loaded_model_json = json_file.read()
+		test_model = model_from_json(loaded_model_json)
+	else:
+		_, _, model = get_model(conf)
+		test_model = model
+
+	test_model.load_weights(filename)
+
+	preds = test_model.predict(np.asarray(inference_vectors), verbose=0)
+	for i in range(len(preds)):
+		print "Sentence %s" % i
+		most_sim_words_list = pairwise_cosine_similarity(preds[i], word_embeddings)
+		# most_sim_words_list = pairwise_cosine_similarity(inference_vectors[i], word_embeddings)
+		print " ".join(inference_sentences[i])
+		sentence = ""
+		for word in most_sim_words_list:
+			sentence += word[0] + " "
+			# sentence += "(" + " ".join(word) + ") "
+		print sentence + "\n"
+
+
+def get_random_data(conf, nb_predictions, embedded_data, string_training_data):
+	random_sentences = []
+	random_vectors = []
+	for _ in range(nb_predictions):
+		index = random.randint(0, len(embedded_data))
+		random_sentences.append(string_training_data[index][:conf.MAX_SEQUENCE_LENGTH])
+		random_vectors.append(embedded_data[index])
+	return random_sentences, random_vectors
+
+
+def train_model(conf, data):
+	decoder, encoder, model = get_model(conf)
+
+	if conf.LOSS == "contrastive_loss":
+		model.compile(metrics=['accuracy'], loss=contrastive_loss, optimizer='adam')
+	else:
+		model.compile(metrics=['accuracy', 'mean_absolute_error', metrics.cosine_proximity, metrics.kullback_leibler_divergence], loss=conf.LOSS, optimizer='adam')
+	filepath, log_dir, log_folder = set_model_name(conf)
+
+	val_gen = batch_generator(data[-conf.VAL_DATA_SIZE:], conf)
+	train_gen = batch_generator(data[:-conf.VAL_DATA_SIZE], conf)
+	if 'SSH_CONNECTION' in os.environ.keys():
+		tensorboard = keras.callbacks.TensorBoard(log_dir='text_generators/logs/' + log_folder, histogram_freq=1,
+												  write_graph=True)
+		es = EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=0, mode='auto')
+		checkpoint = EncoderDecoderModelCheckpoint(decoder, encoder, start_after_epoch=10, filepath=filepath,
+												   monitor='val_loss', verbose=1, save_best_only=True, mode='min',
+												   period=3)
+		callbacks_list = [checkpoint, tensorboard, es]
+		model.fit_generator(generator=train_gen, samples_per_epoch=len(data) - conf.VAL_DATA_SIZE,
+							validation_data=val_gen, nb_val_samples=conf.VAL_DATA_SIZE, nb_epoch=conf.EPOCHS,
+							callbacks=callbacks_list)
+
+		model_json = model.to_json()
+		with open(log_dir + log_folder + "/model.json", "w") as json_file:
+			json_file.write(model_json)
+	else:
+
+		checkpoint = EncoderDecoderModelCheckpoint(decoder, encoder, start_after_epoch=2, filepath=filepath,
+												   monitor='val_loss', verbose=1, save_best_only=True,
+												   mode='min',
+												   period=3)
+		from keras.utils.visualize_util import plot
+		plot(model, show_shapes=True, to_file='text_generators/logs/' + log_folder + "/model.png")
+		model.fit_generator(train_gen, len(data), conf.EPOCHS, callbacks=[checkpoint])
+
+
+def get_model(conf):
+	encoder = Sequential()
+	encoder.add(LSTM(output_dim=conf.HIDDEN_DIM, input_shape=(conf.MAX_SEQUENCE_LENGTH, conf.EMBEDDING_DIMENSION),
+					 return_sequences=False))
+	encoder.add(RepeatVector(conf.MAX_SEQUENCE_LENGTH))  # Get the last output of the RNN and repeats it
+	decoder = Sequential()
+	decoder.add(LSTM(output_dim=conf.HIDDEN_DIM, input_shape=(conf.MAX_SEQUENCE_LENGTH, conf.HIDDEN_DIM),
+					 return_sequences=True))
+	for _ in range(1, conf.DECODER_HIDDEN_LAYERS):
+		decoder.add(LSTM(output_dim=conf.HIDDEN_DIM, return_sequences=True))
+	decoder.add(TimeDistributed(
+		Dense(output_dim=conf.EMBEDDING_DIMENSION, input_shape=(conf.MAX_SEQUENCE_LENGTH, conf.HIDDEN_DIM),
+			  activation=conf.OUTPUT_LAYER_ACTIVATION)))
+	model = Sequential()
+	model.add(encoder)
+	model.add(decoder)
+	model.summary()
+	return decoder, encoder, model
+
+
+def set_model_name(conf):
+	log_folder = "S2S_" + conf.EMBEDDING_METHOD + "_" + str(datetime.now().date()) + "_VS2+" + str(
+		conf.NB_WORDS) + "_BS" + str(conf.BATCH_SIZE) + "_HD" + str(conf.HIDDEN_DIM) + "_DHL" + str(
+		conf.DECODER_HIDDEN_LAYERS) + "_ED" + str(conf.EMBEDDING_DIMENSION) + "_WEM" + conf.WORD_EMBEDDING_METHOD
+	log_dir = "text_generators/logs/"
+	if not os.path.exists(log_dir + log_folder):
+		os.makedirs(log_dir + log_folder)
+	if not os.path.exists(log_dir + log_folder + "/weights"):
+		os.makedirs(log_dir + log_folder + "/weights")
+	print "Working on: %s" % log_folder
+	filepath = log_dir + log_folder + "/weights/E:{epoch:02d}-L:{val_loss:.4f}.hdf5"
+	return filepath, log_dir, log_folder
+
+
+def pairwise_cosine_similarity(predicted_word_vectors, glove_dictionary):
+	glove_vectors = np.asarray(glove_dictionary.values())
+	glove_words = glove_dictionary.keys()
+
+	# glove_vectors.append(np.zeros(300))
+	# glove_words.append("0")
+
+	cos_dis_matrix = cosine_similarity(predicted_word_vectors, glove_vectors)
+
+	predicted_word_vector_count = len(predicted_word_vectors)
+
+	all_word_vectors_count = len(glove_vectors)
+	most_similar_words_list = []
+	for predicted_image_index in range(predicted_word_vector_count):
+		similarities = []
+		for glove_vector_index in range(all_word_vectors_count):
+			glove_word = glove_words[glove_vector_index]
+			cos_sim = cos_dis_matrix[predicted_image_index][glove_vector_index]
+			similarities.append((glove_word, cos_sim))
+		similarities.sort(key=lambda s: s[1], reverse=True)
+		most_similar_words = [x[0] for x in similarities[:5]]
+		most_similar_words_list.append(most_similar_words)
+
+	return most_similar_words_list
+
+
+def get_flickr_sentences():
+	path = "data/datasets/Flickr30k.txt"
+	sentence_file = open(path)
+	word_captions = sentence_file.readlines()
+	sentence_file.close()
+	word_captions = [(line.split("\t")[1]).strip() for line in word_captions]
+	return word_captions
+
+
+def generate_embedding_captions(conf):
+	print "Loading Flickr sentences..."
+	sentences = get_flickr_sentences()
+	word_list_sentences = []
+	for sentence in sentences:
+		if conf.WORD_EMBEDDING_METHOD == "word2vec":
+			word_list = ["<sos>"]
+		else:
+			word_list = []
+		for word in sentence.split(" "):
+			word_list.append(word.lower())
+		if conf.WORD_EMBEDDING_METHOD == "word2vec":
+			word_list.append("<eos>")
+		word_list_sentences.append(word_list)
+
+	word_embedding_dict = get_word_embeddings(conf)
+	return np.asarray(word_list_sentences), word_embedding_dict
+
+
+def emb_get_training_batch(training_batch, word_embedding_dict, conf):
+	embedding_lists = []
+	for word_list in training_batch:
+		embedding_sentence = []
+		for word_string in word_list:
+			if word_string in word_embedding_dict:
+				word_embedding = word_embedding_dict[word_string]
+				embedding_sentence.append(word_embedding)
+		if len(embedding_sentence) > conf.MAX_SEQUENCE_LENGTH:
+			embedding_sentence = embedding_sentence[:conf.MAX_SEQUENCE_LENGTH]
+		while len(embedding_sentence) < conf.MAX_SEQUENCE_LENGTH:
+			zeros = np.zeros(conf.EMBEDDING_DIMENSION)
+			embedding_sentence.insert(0, zeros)
+		embedding_lists.append(embedding_sentence)
+	return np.asarray(embedding_lists)
+
+
+def seq2seq(train=True):
+	conf = W2VEmbToEmbConf
+
+	string_training_data, word_embedding_dict = generate_embedding_captions(conf)
+	embedded_data = emb_get_training_batch(string_training_data, word_embedding_dict, conf)
+	if train:
+		np.random.shuffle(embedded_data)
+		train_model(conf, embedded_data)
+
+	else:
+		model_filename = "S2S_2EMB_2017-03-28_VS2+1000_BS128_HD30_DHL1_ED50_WEMword2vec"
+		weights_filename = "E:170-L:0.0102.hdf5"
+		sample_string_data, sample_embedded_data = get_random_data(conf, 10, embedded_data[-conf.VAL_DATA_SIZE:], string_training_data[-conf.VAL_DATA_SIZE:])
+		inference(conf, sample_string_data, sample_embedded_data, model_filename, weights_filename, word_embedding_dict)

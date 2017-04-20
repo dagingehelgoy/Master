aaca54a712aacd12ac5e2cb06102f20b6304e75d
Markus Lund <markuslund92@hotmail.com>
2017-04-20 15:42:31 +0200

    Remove batch printing

--- GAN/trainer.py
+++ GAN/trainer.py
@@ -67,8 +67,8 @@ def train(gan_logger, config):
 		print("Epoch: %s" % epoch_cnt)
 		np.random.shuffle(all_raw_caption_data)
 		for batch_counter in range(nb_batches):
-			if batch_counter % 10 == 0:
-				print_progress(batch_counter, nb_batches, prefix="Training batches")
+			# if batch_counter % 10 == 0:
+			# 	print_progress(batch_counter, nb_batches, prefix="Training batches")
 			raw_caption_training_batch = all_raw_caption_data[batch_counter * config[Conf.BATCH_SIZE]:(batch_counter + 1) * config[Conf.BATCH_SIZE]]
 
 			if config[Conf.IMAGE_CAPTION]:

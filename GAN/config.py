7d48da852ddb0325ac15d6c116b1639f83aa12d6
Markus Lund <markuslund92@hotmail.com>
2017-04-24 16:19:51 +0200

    Change model to train 00058.txt

--- GAN/config.py
+++ GAN/config.py
@@ -17,13 +17,14 @@ config = {
 
 	Conf.DATE: datetime.datetime.now().date(),
 
-	# Conf.NAME_SUFFIX: None,
-	Conf.NAME_SUFFIX: "dropout0.25",
+	Conf.NAME_SUFFIX: None,
+	# Conf.NAME_SUFFIX: "dropout0.25",
 
 	Conf.VOCAB_SIZE: 1000,
-	Conf.MAX_SEQ_LENGTH: 5,
+	Conf.MAX_SEQ_LENGTH: 10,
 
-	Conf.LIMITED_DATASET: "flowers",
+	# Conf.LIMITED_DATASET: "flowers",
+	Conf.LIMITED_DATASET: "00058.txt",
 	# Conf.LIMITED_DATASET: None,
 	Conf.DATASET_SIZE: -1,
 	Conf.BATCH_SIZE: 10,
@@ -34,13 +35,14 @@ config = {
 	Conf.MAX_LOSS_DIFF: 0,
 
 	Conf.EMBEDDING_SIZE: 50,
-	Conf.NOISE_SIZE: 40,
+	Conf.NOISE_SIZE: 50,
 	Conf.PREINIT: PreInit.NONE,
 
 	Conf.WORD2VEC_NUM_STEPS: 100001,
 
-	Conf.MODELNAME: "2017-04-22_ImgCapFalse_word2vec_Vocab1000_Seq5_Batch10_EmbSize50_repeat_Noise40_PreInitNone_Dataset_surf_dropout0.25",
-	# Conf.MODELNAME: None,
+	# Conf.MODELNAME: "2017-04-22_ImgCapFalse_word2vec_Vocab1000_Seq5_Batch10_EmbSize50_repeat_Noise40_PreInitNone_Dataset_surf_dropout0.25",
+	Conf.MODELNAME: None,
+
 	Conf.IMAGE_CAPTION: False,
 	Conf.IMAGE_DIM: 50
 

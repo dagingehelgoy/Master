95b749e2f9e5c67eace828ad2d642eb162573548
Markus Lund <markuslund92@hotmail.com>
2017-05-03 11:07:03 +0200

    Fix loading of wrong word2vec dict

--- GAN/config.py
+++ GAN/config.py
@@ -17,8 +17,8 @@ config = {
 
 	Conf.DATE: datetime.datetime.now().date(),
 
-	Conf.NAME_SUFFIX: None,
-	# Conf.NAME_SUFFIX: "dropout0.5",
+	# Conf.NAME_SUFFIX: None,
+	Conf.NAME_SUFFIX: "two_flowers_image_caption",
 	# Conf.NAME_SUFFIX: "sam1",
 
 	Conf.VOCAB_SIZE: 1000,

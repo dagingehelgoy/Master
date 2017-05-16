a2eb90f9d05287db6999bffeff5b12d0c6568d89
Markus Lund <markuslund92@hotmail.com>
2017-05-16 10:01:55 +0200

    GAN - image caption all PCA red/yellow

--- GAN/config.py
+++ GAN/config.py
@@ -18,8 +18,8 @@ config = {
 
 	Conf.DATE: datetime.datetime.now().date(),
 
-	Conf.NAME_SUFFIX: None,
-	# Conf.NAME_SUFFIX: "0.75dropout-softmax",
+	# Conf.NAME_SUFFIX: None,
+	Conf.NAME_SUFFIX: "all_pca",
 
 	Conf.VOCAB_SIZE: 1000,
 	Conf.MAX_SEQ_LENGTH: 15,
